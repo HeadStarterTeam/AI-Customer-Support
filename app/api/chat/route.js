973f41e4ae2489e1cbd92ceebaf73791c2d75720
Enrico Zamarripa <ricozama121803@gmail.com>
2024-08-08 13:01:34 -0700

    Initial commit or appropriate message

---
+++ app/api/chat/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `Welcome to HeadstarterAI Support! I'm here to help you navigate through your AI-powered interview process. Whether you need assistance with setting up your interview, technical support, or have questions about how to best prepare, just type your query below.
+
+If you're ready to start, you can also say "Begin interview," and I'll guide you through the process. How can I assist you today?`;
+
+export async function POST(req) {
+    const openai = new OpenAI();
+    const data = await req.json();
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: systemPrompt,
+            },
+            ...data,
+        ],
+        model: 'gpt-4.0-mini',
+        stream: true,
+    });
+
+    const stream = new ReadableStream({
+        async start(controller) {
+            const encoder = new TextEncoder();
+            try {
+                for await (const chunk of completion) {
+                    const content = chunk.choices[0]?.delta?.content;
+                    if (content) {
+                        const text = encoder.encode(content);
+                        controller.enqueue(text);
+                    }
+                }
+            } catch (err) {
+                controller.error(err);
+            } finally {
+                controller.close();
+            }
+        },
+    })
+
+    return new NextResponse(stream)
+}
